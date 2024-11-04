b5f52ff79842c6734ed8bccd13bd42c22bf721c6
doubleliiiii <doubleli@bupt.edu.cn>
2024-11-04 10:03:34 +0800

    first commmit

---
+++ src/test/java/com/example/callous/CallousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.callous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallousApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
